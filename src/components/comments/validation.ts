8eaeb1d69792fc11d20a690c6bb2b8cdee183dfe
Polina Balazhy <71522782+High-Voltaged@users.noreply.github.com>
2024-05-21 18:05:32 +0300

    feat: field validation completion

--- src/components/comments/validation.ts
+++ src/components/comments/validation.ts
@@ -1,9 +1,10 @@
 import * as yup from "yup";
 
 const CONTENT_MIN_LIMIT = 4;
+const CONTENT_MAX_LIMIT = 200;
 
 const addCommentSchema = yup.object().shape({
-  content: yup.string().min(CONTENT_MIN_LIMIT).required("Content is required"),
+  content: yup.string().min(CONTENT_MIN_LIMIT).max(CONTENT_MAX_LIMIT).required("Content is required"),
 });
 
 export default addCommentSchema;
--- src/components/shared/Skills/validation.ts
+++ src/components/shared/Skills/validation.ts
@@ -1,9 +1,10 @@
 import * as yup from "yup";
 
 const NAME_MIN_LIMIT = 2;
+const NAME_MAX_LIMIT = 20;
 
 const addSkillSchema = yup.object().shape({
-  name: yup.string().min(NAME_MIN_LIMIT).required("Name is required"),
+  name: yup.string().min(NAME_MIN_LIMIT).max(NAME_MAX_LIMIT).required("Name is required"),
 });
 
 export default addSkillSchema;
